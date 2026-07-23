d7f47cc0d765cf18ea98297275cbbbfcc0ea0707
Andrew McGuire <amtektonics@gmail.com>
2026-07-22 21:41:48 -0400

    skeleton of the world
    I'm probably overthinking a lot of this but I think I have a good enough idea to call it for tonight.

---
+++ scenes/game_world/game_world.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+var _active_room:GameRoom = null
+
+@export_node_path("Node3D") var _player := NodePath("Player")
---
+++ scenes/game_world/rooms/game_room.gd
@@ -0,0 +1,4 @@
+extends Node3D
+class_name GameRoom
+
+@onready var _player_positions_node = $PlayerPositions
